c148cae84c1b5df3d552d338374fcae5c0a1180b
Frank Moran <info@francisfrankmoran.com>
2020-02-26 13:37:06 -0500

    ok

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
 
 // API Routes
 // app.get("/api/workouts", (req,res) => {
